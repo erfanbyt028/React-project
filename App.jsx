7ee19a15a4653f103e31eea080e9fcc80164cca8
erfanbyt028 <150285997+erfanbyt028@users.noreply.github.com>
2025-03-01 23:22:40 +0330

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Components/Header';
+import Product from "./Components/Product";
+// import './App.css'; // اگر نیازی به فایل CSS ندارید، این خط را حذف کنید
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Product/>
+    </>
+  );
+}
+
+export default App;
